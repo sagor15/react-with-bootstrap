b47372caefef59295c19d6929924a6ca7a4fc9ea
sagor20202021 <sagorahammed1562@gamil.com>
2022-03-27 23:45:49 +0600

    added button bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>React with bootstrap</h1>
+      <>
+        <Button variant="outline-primary">Primary</Button>{" "}
+        <Button variant="outline-secondary">Secondary</Button>{" "}
+        <Button variant="outline-success">Success</Button> <Button variant="outline-warning">Warning</Button>{" "}
+        <Button variant="outline-danger">Danger</Button> <Button variant="outline-info">Info</Button>{" "}
+        <Button variant="outline-light">Light</Button> <Button variant="outline-dark">Dark</Button>
+      </>
     </div>
   );
 }
